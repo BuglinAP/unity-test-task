df2b5789fd597d5c2d09f73c7a6da726d1e02b5e
Andrey Buglin <43354253+BuglinAP@users.noreply.github.com>
2023-06-17 14:11:49 +0300

    Input System Added

---
+++ Test Task/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField] public Camera playerCamera;
+    [SerializeField] private float xRotation = 0f;
+    [SerializeField] private float xSensitivity = 30f;
+    [SerializeField] private float ySensitivity = 30f;
+
+    public void ProcessLook(Vector2 input)
+    {
+        float mouseX = input.x;
+        float mouseY = input.y;
+
+        xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
+        xRotation = Mathf.Clamp(xRotation, -20f, 20f);
+
+        playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.Rotate((mouseX * Time.deltaTime) * xSensitivity * Vector3.up);
+    }
+}
